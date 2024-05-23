791674cf7747b494772f1b2a95bc9e6ba5b927fd
DevTae <dev.taehyeon.kim@gmail.com>
2024-05-23 21:14:58 +0900

    Init project

---
+++ src/test/java/kr/co/dbinc/BackDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.dbinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
