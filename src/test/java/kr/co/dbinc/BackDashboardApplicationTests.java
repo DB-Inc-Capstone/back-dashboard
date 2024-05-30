33757b9f9deb21f7844e5183329c54e90778c96f
DevTae <dev.taehyeon.kim@gmail.com>
2024-05-30 11:01:08 +0900

    Update test code

--- src/test/java/kr/co/dbinc/BackDashboardApplicationTests.java
+++ src/test/java/kr/co/dbinc/BackDashboardApplicationTests.java
@@ -3,10 +3,10 @@ package kr.co.dbinc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BackDashboardApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
